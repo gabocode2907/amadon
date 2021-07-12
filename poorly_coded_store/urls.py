d1f7416bc4df76c7d7f83af9e4689906978c4633
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-07-11 21:34:28 -0500

    update!

--- poorly_coded_store/urls.py
+++ poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('checkout/', views.checkout)
+    path('checkout/', views.checkout),
+    path('success/<int:number>/',views.success)
 ]
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.db.models.expressions import Value
+from django.db.models.fields import PositiveBigIntegerField
+from django.shortcuts import redirect, render
 from .models import Order, Product
+from django.db.models import Sum
+
 
 def index(request):
     context = {
@@ -9,8 +13,23 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    get_product_price = Product.objects.get(id=request.POST["id"])
+    price_from_form = float(get_product_price.price)
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    new_order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    
+    return redirect("/success/"+str(new_order.id))
+
+def success(request,number):
+    submitted_order = Order.objects.get(id=number)
+    quantity_from_form = submitted_order.quantity_ordered
+    total_charge = submitted_order.total_price
+    grand_total = Order.objects.aggregate(total_cost=Sum('total_price'))
+    context = {
+        'b_quantity' : quantity_from_form,
+        'b_total' : total_charge,
+        'grand_total' : grand_total,
+        'total_items' : Order.objects.aggregate(total_items=Sum('quantity_ordered'))
+    }
+    return render(request, "store/checkout.html",context)
